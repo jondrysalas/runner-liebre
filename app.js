312ab0eecf7adf8944d06caaa2d22b4472f73649
jondrysalas <jondrisalas10@gmail.com>
2021-06-23 20:22:19 -0300

    .ignore

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en el puerto 3000");
+})
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    let htmlPath = path.join(__dirname, "/views/home.html");
+    res.sendFile(htmlPath);
+})
+
+app.get("/crear-tu-cuenta", function(req, res){
+    let htmlPath = path.join(__dirname, "/views/register.html");
+    res.sendFile(htmlPath);
+})
